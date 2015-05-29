c412a425287b656c32fa6d4a290c2b1d3dd45d7d
toozoofoo <toozoofoo@gmail.com>
2015-05-29 15:24:16 +0800

    do not convert to list

--- django_comments/templatetags/comments.py
+++ django_comments/templatetags/comments.py
@@ -110,7 +110,7 @@ class BaseCommentNode(template.Node):
 class CommentListNode(BaseCommentNode):
     """Insert a list of comments into the context."""
     def get_context_value_from_queryset(self, context, qs):
-        return list(qs)
+        return qs
 
 class CommentCountNode(BaseCommentNode):
     """Insert a count of comments into the context."""
